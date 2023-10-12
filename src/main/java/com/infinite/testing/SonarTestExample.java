e26b54f7739fae316352886ef56789c1f61df0ce
akshayap <akshayap@10.4.33.57>
2023-10-12 11:12:50 +0530

    sonarcommit5

--- src/main/java/com/infinite/testing/SonarTestExample.java
+++ src/main/java/com/infinite/testing/SonarTestExample.java
@@ -2,9 +2,11 @@ package com.infinite.testing;
 
 public class SonarTestExample {
 	public String b;
+	public String a;
 
 	public static void main(String args[]) {
 		System.out.println("b");
+		System.out.println("a");
 
 }
 }
